4ed19832453dd838bfc1eacde9929c34bc8440c1
Rahmat <rahmat.sumantri@romindo.net>
2023-03-09 08:39:44 +0700

    dashboard

--- resources/views/dashboard/user.blade.php
+++ resources/views/dashboard/user.blade.php
@@ -10,6 +10,84 @@
             </div>
 
             <div class="section-body">
+                <div class="row">
+                    <div class="col-lg-6 col-md-6 col-12">
+                        <div class="card card-hero">
+                            <div class="card-header bg-info">
+                                <div class="card-icon">
+                                    <i class="far fa-clipboard"></i>
+                                </div>
+                                <h3>Production</h3>
+                            </div>
+                            <div class="card-body p-0">
+                                <div class="tickets-list">
+                                    <a href="#" class="ticket-item">
+                                        <div class="ticket-title">
+                                            <h4>Job Order (OPR)</h4>
+                                            <div class="badge badge-pill badge-info">0</div>
+                                        </div>
+                                    </a>
+                                    <a href="#" class="ticket-item">
+                                        <div class="ticket-title">
+                                            <h4>Job Order Inquiry Process</h4>
+                                            <div class="badge badge-pill badge-info">0</div>
+                                        </div>
+                                    </a>
+                                    <a href="#" class="ticket-item">
+                                        <div class="ticket-title">
+                                            <h4>Bill Of  Material</h4>
+                                            <div class="badge badge-pill badge-info">0</div>
+                                        </div>
+                                    </a>
+                                    <a href="#" class="ticket-item">
+                                        <div class="ticket-title">
+                                            <h4>Buat BPHP</h4>
+                                            <div class="badge badge-pill badge-info">0</div>
+                                        </div>
+                                    </a>
+                                    <a href="#" class="ticket-item">
+                                        <div class="ticket-title">
+                                            <h4>Release BPHP</h4>
+                                            <div class="badge badge-pill badge-info">0</div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-12">
+                        <div class="card card-hero">
+                            <div class="card-header bg-primary">
+                                <div class="card-icon">
+                                    <i class="fas fa-tasks"></i>
+                                </div>
+                                <h3>Purchasing</h3>
+                            </div>
+                            <div class="card-body p-0">
+                                <div class="tickets-list">
+                                    <a href="#" class="ticket-item">
+                                        <div class="ticket-title">
+                                            <h4>Purchase Order</h4>
+                                            <div class="badge badge-pill badge-info">0</div>
+                                        </div>
+                                    </a>
+                                    <a href="#" class="ticket-item">
+                                        <div class="ticket-title">
+                                            <h4>LPB</h4>
+                                            <div class="badge badge-pill badge-info">0</div>
+                                        </div>
+                                    </a>
+                                    <a href="#" class="ticket-item">
+                                        <div class="ticket-title">
+                                            <h4>Release QC</h4>
+                                            <div class="badge badge-pill badge-info">0</div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     </div>
